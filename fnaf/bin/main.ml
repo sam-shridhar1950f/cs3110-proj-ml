d16c66bc54f0c2620f81fd3bf8b917bdad71fc4a
Larry <ltt28@cornell.edu>
2024-03-27 10:35:27 -0400

    merging changes

--- fnaf/bin/main.ml
+++ fnaf/bin/main.ml
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 include Random
 [@@@ocaml.warning "-69"]
 type game_state = {
@@ -20,3 +21,95 @@ let initial_state () = {
 }
 
 
+=======
+open Graphics
+let clear_window color =
+  let fg = foreground in
+  set_color color;
+  fill_rect 0 0 (size_x ()) (size_y ());
+  set_color fg
+
+
+let new_game () = 
+  clear_window black;
+  set_color white;
+  moveto 500 500;
+  draw_string "Welcome to Five Days In Office Hours.";
+  moveto 500 300;
+  draw_string "Start Game: s";
+  let e = wait_next_event [ Key_pressed ] in
+  let user_command =
+    match e.key with
+    | ';' -> close_graph ()
+    | _ -> ()
+  in
+  user_command
+  
+let load_game () = 
+  clear_window black;
+  set_color white;
+  moveto 500 500;
+  draw_string "Select a game to load.";
+  let e = wait_next_event [ Key_pressed ] in
+  let user_command =
+    match e.key with
+    | ';' -> close_graph ()
+    | _ -> ()
+  in
+  user_command
+
+  let settings () = 
+    clear_window black;
+    set_color white;
+    moveto 500 500;
+    draw_string "Game Settings";
+    let e = wait_next_event [ Key_pressed ] in
+    let user_command =
+      match e.key with
+      | ';' -> close_graph ()
+      | _ -> ()
+    in
+    user_command
+  
+let start_screen () = 
+  clear_window black;
+  set_color white;
+  moveto 200 600;
+  draw_string "New Game: n";
+  moveto 200 500;
+  draw_string "Load Game: l";
+  moveto 200 400;
+  draw_string "Settings: s";
+  let e = wait_next_event [ Key_pressed ] in
+  let user_command =
+    match e.key with
+    | ';' -> close_graph ()
+    | 'n' -> new_game ()
+    | 'l' -> load_game ()
+    | 's' -> settings ()
+    | _ -> ()
+  in
+  user_command
+let loading_screen () = 
+  open_graph " 1280x780";
+  open_graph "";
+  clear_window black;
+  set_color yellow;
+  moveto 500 150;
+  draw_string "Press [ s ] to start... ";
+  moveto 650 20;
+  set_color white;
+  draw_string
+    "Developed by Larry Tao, Sam Shridhar, Rohan Mahajan, Jacob Huang";
+  synchronize ();
+  let e = wait_next_event [ Key_pressed ] in
+    if e.key = ';' then close_graph ()
+    else if e.key = 's' then start_screen ()
+let main_menu () =
+  loading_screen ();
+  synchronize ();
+  close_graph ();;
+
+
+let () = main_menu ()
+>>>>>>> 0c6fd52 (implemented basic gui in bin/main.ml)
